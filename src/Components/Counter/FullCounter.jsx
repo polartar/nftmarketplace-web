7d438c3365f244c06254309856549c1295895f81
Nathan Schwermann <schwiz@gmail.com>
2021-10-31 02:45:22 -0500

    add back style sheet

--- src/Components/Counter/FullCounter.jsx
+++ src/Components/Counter/FullCounter.jsx
@@ -1,5 +1,6 @@
 import { Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
+import './Counter.css'
 
 const FullCounter = ({ hideImg}) => {
   const [timeLeft, setTimeLeft] = useState({});
