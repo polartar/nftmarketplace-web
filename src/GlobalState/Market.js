80981141199c5b1e5cd019e17954cc099aa763ed
Nathan Schwermann <schwiz@gmail.com>
2021-12-08 15:37:58 -0600

    fix parse api resposne

--- src/GlobalState/Market.js
+++ src/GlobalState/Market.js
@@ -104,27 +104,14 @@ export const init = (state, type, address) => async(dispatch) => {
         const rawResponse = await sortAndFetch('Listing ID', 1, type, address);
         const pages = rawResponse.totalPages;
         const listingsResponse = rawResponse.listings.map((e) => {
-            const nft = {
-                'name' : e.name,
-                'image' : e.image,
-                'description' : e.description,
-                'properties' : (e.properties) ? e.properties : []
-            }
             return {
                 ...e,
                 'listingId': ethers.BigNumber.from(e.listingId),
                 'price' : ethers.utils.parseEther(String(e.price)),
-                'nft' : nft
             }
-        }).filter(e => typeof e.name !== 'undefined'); //backend hasn't fetched metadata for this listing
+        }); //backend hasn't fetched metadata for this listing
 
-        // if(type === 'collection'){
-        //     listingsResponse = listingsResponse.filter((e) => e.nftAddress.toLowerCase() === address.toLowerCase());
-        // } else if(type === 'seller'){
-        //     listingsResponse = listingsResponse.filter((e) => e.seller.toLowerCase() === address.toLowerCase());
-        // }
-        // const pages = Math.ceil(listingsResponse.length / pagesize);
-        // listingsResponse = sortByType(listingsResponse, state.market.sortOrder);
+
         const listings = new Array(pages);
         listings[1] = listingsResponse
         
@@ -133,36 +120,22 @@ export const init = (state, type, address) => async(dispatch) => {
             'totalPages' : pages,
             'listings' : new Array(pages),
             'address': address,
-            // 'response' : listingsResponse
         }))
 }
 
-/**
- * 
-                'name': name,
-                'image' : image,
-                'description' : description,
-                'properties' : (properties) ? properties : [],
- */
 
 export const loadPage = (page, type, address, order) => async(dispatch) => {
     ///TODO show loaded
     //dispatch(startLoading())
     const rawResponse = await sortAndFetch(order, page, type, address);
     const listingsResponse =  rawResponse.listings.map((e) => {
-        const nft = {
-            'name' : e.name,
-            'image' : e.image,
-            'description' : e.description,
-            'properties' : (e.properties) ? e.properties : []
-        }
+
         return {
             ...e,
             'listingId': ethers.BigNumber.from(e.listingId),
             'price' : ethers.utils.parseEther(String(e.price)),
-            'nft' : nft
         }
-    }).filter(e => typeof e.name !== 'undefined');
+    });
     // const index = (page - 1) * pagesize;
     // let listings = [...state.market.response].splice(index, pagesize);
     dispatch(onNewPage({
