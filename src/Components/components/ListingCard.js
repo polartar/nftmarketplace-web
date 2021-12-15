8b138ee3feb330fbdf5c63ccef0646cdc6dbd620
Rob Cunning <creative@cunning.ca>
2021-12-15 15:32:29 -0700

    make preview images clickable

--- src/Components/components/ListingCard.js
+++ src/Components/components/ListingCard.js
@@ -29,7 +29,7 @@ const ListingCard = ({ listing, className = 'd-item col-lg-3 col-md-6 col-sm-6 c
                 <div className="nft__item_wrap" style={{height: `${height}px`}}>
                     <Outer>
                         <span>
-                            <img onLoad={onImgLoad} src={listing.nft.image} className="lazy nft__item_preview" alt=""/>
+                            <img onClick={() => navigateTo(`/listing/${listing.listingId}`)} onLoad={onImgLoad} src={listing.nft.image} className="lazy nft__item_preview" alt=""/>
                         </span>
                     </Outer>
                 </div>
