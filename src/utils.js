f2fe425bb587a99c091304a7b67cc800e472c413
Rob Cunning <creative@cunning.ca>
2021-12-19 10:33:46 -0700

    add a function to convert stats to use SI prefixes

--- src/utils.js
+++ src/utils.js
@@ -150,3 +150,22 @@ export function humanize(str) {
   }
   return frags.join(' ').split(/(?=[A-Z])/).join(' ');
 }
+
+export function siPrefixedNumber(num) {
+
+  // Nine Zeroes for Billions
+  return Math.abs(Number(num)) >= 1.0e+9
+
+      ? (Math.abs(Number(num)) / 1.0e+9).toFixed(2) + "B"
+      // Six Zeroes for Millions
+      : Math.abs(Number(num)) >= 1.0e+6
+
+          ? (Math.abs(Number(num)) / 1.0e+6).toFixed(2) + "M"
+          // Three Zeroes for Thousands
+          : Math.abs(Number(num)) >= 1.0e+3
+
+              ? (Math.abs(Number(num)) / 1.0e+3).toFixed(2) + "K"
+
+              : Math.abs(Number(num));
+
+}
