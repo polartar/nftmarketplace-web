2c4d57174052c18df73451b269ad38d8948b0e48
Rob Cunning <creative@cunning.ca>
2021-12-23 18:46:32 -0700

    add in new ebisu front page background

--- src/Components/pages/home.js
+++ src/Components/pages/home.js
@@ -36,12 +36,20 @@ const inline = keyframes`
 `;
 
 const GlobalStyles = createGlobalStyle`
-  .jumbotron h1, .jumbotron h3, .jumbotron h5, .jumbotron p {
-    color: #fff;
+  .header-card {
+    background: #FFFFFFDD;
+    border-radius: 10px;
   }
+    
   .de_count h3 {
     font-size: 36px;
     margin-bottom: 0px;
+  }
+  
+  @media only screen and (max-width: 464px) {
+    .header-card .call-to-action {
+        text-align: center !important
+    }
   }
 `;
 
@@ -65,24 +73,28 @@ const Home = () => {
         <div>
             <GlobalStyles/>
             <section className="jumbotron breadcumb no-bg h-vh"
-                     style={{backgroundImage: `url(${'./img/background/7.jpg'})`}}>
+                     style={{backgroundImage: `url(${'./img/background/Ebisus-bg-1_L.jpg'})`}}>
                 <div className="container">
                     <div className="row align-items-center">
-                        <div className="col-md-6">
+                        <div className="col-lg-6 header-card px-4">
                             <div className="spacer-single"></div>
-                            <h6> <span className="text-uppercase color">Ebisu's Bay Marketplace</span></h6>
+                            <h6>
+                                <span className="text-uppercase color">Ebisu's Bay Marketplace</span>
+                            </h6>
                             <Reveal className='onStep' keyframes={fadeInUp} delay={300} duration={900} triggerOnce>
-                                <h1 className="col-white">Discover <span className="color">rare</span> digital art and collect NFTs</h1>
+                                <h1>Discover <span className="color">rare</span> digital art and collect NFTs</h1>
                             </Reveal>
                             <Reveal className='onStep' keyframes={fadeInUp} delay={600} duration={900} triggerOnce>
-                                <p className="lead col-white">
+                                <p className="lead">
                                     Ebisu's Bay is the first NFT marketplace on Cronos. Create, buy, sell, trade and enjoy the #crofam NFT community
                                 </p>
                             </Reveal>
                             <div className="spacer-10"></div>
-                            <Reveal className='onStep d-inline' keyframes={inline} delay={800} duration={900} triggerOnce>
-                                <span onClick={()=> window.open('/marketplace', "_self")} className="btn-main inline lead">Explore</span>
-                                <span onClick={()=> window.open('https://forms.gle/rRtn6gp16tyavQge9', "_blank")} className="btn-main inline white lead">Become a Creator</span>
+                            <Reveal className='onStep call-to-action' keyframes={inline} delay={800} duration={900} triggerOnce>
+                                <div className="d-inline">
+                                    <span onClick={()=> window.open('/marketplace', "_self")} className="btn-main inline lead mb-2">Explore</span>
+                                    <span onClick={()=> window.open('https://forms.gle/rRtn6gp16tyavQge9', "_blank")} className="btn-main btn-outline inline white lead" style={{outline: '1px solid #DDD'}}>Become a Creator</span>
+                                </div>
                                 <div className="mb-sm-30"></div>
                             </Reveal>
 
@@ -91,21 +103,21 @@ const Home = () => {
                                     <div className="spacer-single"></div>
                                     {marketData &&
                                         <div className="row">
-                                            <div className="col-lg-4 col-md-6 col-sm-4 mb30">
+                                            <div className="col-sm-4 col-6 mb30">
                                                 <div className="de_count text-left">
                                                     <h3><span>{siPrefixedNumber(Number(marketData.totalVolume).toFixed(0))}</span></h3>
                                                     <h5 className="id-color">Volume</h5>
                                                 </div>
                                             </div>
 
-                                            <div className="col-lg-4 col-md-6 col-sm-4 mb30">
+                                            <div className="col-sm-4 col-6 mb30">
                                                 <div className="de_count text-left">
                                                     <h3><span>{siPrefixedNumber(Number(marketData.totalSales).toFixed(0))}</span></h3>
                                                     <h5 className="id-color">NFTs Sold</h5>
                                                 </div>
                                             </div>
 
-                                            <div className="col-lg-4 col-md-6 col-sm-4 mb30">
+                                            <div className="col-sm-4 col-6 mb30">
                                                 <div className="de_count text-left">
                                                     <h3><span>{siPrefixedNumber(marketData.totalActive)}</span></h3>
                                                     <h5 className="id-color">Active Listings</h5>
@@ -115,9 +127,6 @@ const Home = () => {
                                     }
                                 </div>
                             </Reveal>
-                        </div>
-                        <div className='col-md-6 px-0 xs-hide'>
-                            <HomeCarousel/>
                         </div>
                     </div>
                 </div>
