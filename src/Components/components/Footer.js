de9126dac0032be55c219121225a50ead544cf17
Eric Askari <eric.askari@weare.fi>
2022-01-12 08:57:11 +0200

    [WEB-369] Refactor: Footer style improvement

--- src/Components/components/Footer.js
+++ src/Components/components/Footer.js
@@ -8,18 +8,18 @@ const Footer = () => {
 
 
     return (
-        <footer className="footer-light" data-is-in-home-page={(location.pathname === '/').toString()}>
+        <footer className="footer-light" data-is-in-home-page={ (location.pathname === '/').toString() }>
             <div className="container text-center">
                 <h5>Partners</h5>
                 <div className="row">
                     <div className="col">
                         <a href="https://nebkas.ro" target="_blank">
-                            <img src={nebkas} alt='nebkas.co' width='128px'/>
+                            <img src={ nebkas } alt='nebkas.co' width='128px'/>
                         </a>
                     </div>
                     <div className="col">
                         <a href="https://www.weare.fi/" target="_blank">
-                            <img src={weAreFi} alt='WeAre Solutions' width='64px'/>
+                            <img src={ weAreFi } alt='WeAre Solutions' width='64px'/>
                         </a>
                     </div>
                 </div>
@@ -27,34 +27,32 @@ const Footer = () => {
 
             <div className="subfooter">
                 <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">
-                            <div className="de-flex">
-                                <div className="de-flex-col">
-                                    <span onClick={()=> window.open("", "_self")}>
-                                        <img
-                                            height="40px"
-                                            src="/img/web_logo.svg"
-                                            alt="#"
-                                        />
-                                        <span className="copy">Copyright &copy; 2021 EbisusBay.com. All rights reserved</span>
-                                    </span>
-                                    <a href="/tos.html" target="_blank">
-                                        &nbsp;{'| Terms of Service'}
-                                    </a>
-                                </div>
-                                <div className="de-flex-col">
-                                    <div className="social-icons">
-                                        <span onClick={()=> window.open("https://twitter.com/EbisusBay", "_blank")}><i className="fa fa-twitter fa-lg"></i></span>
-                                    </div>
-                                </div>
-                            </div>
+                    <div className="row align-items-center">
+                        <div className='col-2'>
+                            <img height="40px"
+                                 src="/img/web_logo.svg"
+                                 alt="#"
+                            />
                         </div>
+
+                        <div className="col-10 social-icons d-flex justify-content-end">
+                            <span onClick={ () => window.open("https://twitter.com/EbisusBay", "_blank") }>
+                                <i className="fa fa-twitter fa-lg"/>
+                            </span>
+                        </div>
+
+                        <a href="/tos.html" target="_blank" className='col-12 pt-3 text-center'>
+                            &nbsp;{ 'Terms of Service' }
+                        </a>
+
+                        <span className="col-12 pt-3 copy text-center" style={{opacity: '0.6'}}>
+                            Copyright &copy; 2021 EbisusBay.com. All rights reserved
+                        </span>
                     </div>
                 </div>
             </div>
         </footer>
-    )
+    );
 };
 
 export default memo(Footer);
