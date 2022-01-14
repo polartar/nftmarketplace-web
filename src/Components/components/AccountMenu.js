89988c2e8489a539653190a5260db7287f1cfe22
Rob Cunning <creative@cunning.ca>
2022-01-14 01:00:41 -0700

    [WEB-228] add CRO balance to account menu and rename market balance to market escrow

--- src/Components/components/AccountMenu.js
+++ src/Components/components/AccountMenu.js
@@ -18,7 +18,7 @@ import {toast} from "react-toastify";
 import MetaMaskOnboarding from '@metamask/onboarding';
 import { nanoid } from 'nanoid'
 import {ethers} from 'ethers'
-import { Modal, NavLink } from "react-bootstrap";
+import {Modal, NavLink, Spinner} from "react-bootstrap";
 import { createSuccessfulTransactionToastContent } from "../../utils";
 
 const AccountMenu = function() {
@@ -192,7 +192,33 @@ const AccountMenu = function() {
                             </div>
                         </div>
                         <div className="d-wallet">
-                            <h4>Market Balance</h4>
+                            <h4>CRO Balance</h4>
+                            <div className="d-flex justify-content-between">
+                                <span>
+                                    {!user.connectingWallet ?
+                                        <span>
+                                            {user.balance ?
+                                                <>
+                                                    {Math.round(user.balance * 100) / 100} CRO
+                                                </>
+                                                :
+                                                <>
+                                                    N/A
+                                                </>
+                                            }
+                                        </span>
+                                        :
+                                        <span>
+                                            <Spinner animation="border" role="status" size={"sm"}>
+                                                <span className="visually-hidden">Loading...</span>
+                                            </Spinner>
+                                        </span>
+                                    }
+                                </span>
+                            </div>
+                        </div>
+                        <div className="d-wallet">
+                            <h4>Market Escrow</h4>
                             <div className="d-flex justify-content-between">
                                 <span>{user.marketBalance} CRO</span>
                                 {user.marketBalance !== '0.0' &&
